41e58d1a98b78570aba79a4a9f45eb64ffd59e8d
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2026-05-16 11:22:11 +0530

    chore(web): remove default vite files and rules

---
+++ apps/web/src/router.tsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from 'react-router-dom';
+import type { RouteObject } from 'react-router-dom';
+
+const routes: RouteObject[] = [];
+
+const router = createBrowserRouter(routes);
+
+export default router;
