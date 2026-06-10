f1fccd312491bfd63439752c8e7fd63233fc6f40
Yashu Lilhare <182187926+yashu483@users.noreply.github.com>
2026-06-10 22:53:19 +0530

    setup tanstack react query for server queries

--- apps/web/src/components/ui/Logo.tsx
+++ apps/web/src/components/ui/Logo.tsx
@@ -1,12 +1,13 @@
 import { cn } from '@/utils/cn';
 import logoImg from '@/assets/icons/logo.png';
+import { Link } from 'react-router-dom';
 
 const Logo = () => {
   return (
-    <div className={cn('flex gap-2 items-center pt-2')}>
+    <Link to={'/'} className={cn('flex gap-2 items-center pt-2')}>
       <img src={logoImg} className={cn('w-8 aspect-square')} />
       <h1 className={cn('m-0 text-gray-800 ')}>Odin Messenger</h1>
-    </div>
+    </Link>
   );
 };
 
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,9 +4,14 @@ import './index.css';
 
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const client = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
